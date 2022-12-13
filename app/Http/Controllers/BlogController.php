6dae16b248916178ae1c2598ded6eb0be373c641
tifarin <latifahlatifah443@gmail.com>
2022-12-13 14:19:42 +0700

    add detail

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Session;
 use Illuminate\Http\Request;
 use App\Models\Blog;
 
@@ -11,5 +11,60 @@ class BlogController extends Controller
     {
         $blog = Blog::all();
         return view('blog.index', ['blogList' => $blog]);
+    }
+    public function create()
+    {
+        $blog = Blog::all();
+        return view('blog.create', ['blogList' => $blog]);
+    }
+    public function store(Request $request)
+    {
+        $newName = "";
+        $extension = $request->file('photo')->getClientOriginalExtension();
+        $newName = $request->title.'-'.now()->timestamp.'.'.$extension;
+        $request->file('photo')->storeAs('blog',$newName);
+        $request['cover']=$newName;
+
+        $blog = Blog::create($request->all());
+        if($blog) {
+            Session::flash('status', 'success');
+            Session::flash('message', 'add new blog success !');
+        }
+        return redirect('/blog/');
+    }
+    public function detail($id) {
+        $blog = Blog::findOrFail($id);
+        return view('blog.detail', ['blog' => $blog]);
+    }
+    public function edit($id) {
+        $blog = Blog::findOrFail($id);
+        return view('blog.edit', ['blog' => $blog]);
+    }
+    public function update(Request $request, $id)
+    {
+        if($request->file('photo')) {
+        $newName = "";
+        $extension = $request->file('photo')->getClientOriginalExtension();
+        $newName = $request->title.'-'.now()->timestamp.'.'.$extension;
+        $request->file('photo')->storeAs('blog',$newName);
+        $request['cover']=$newName;
+        }
+
+        $blog = Blog::findOrFail($id);
+        $blog->update($request->all());
+        if($blog) {
+            Session::flash('status', 'success');
+            Session::flash('message', 'update blog success !');
+        }
+        return redirect('/blog/');
+    }
+    function delete($id) {
+        $blog = Blog::findOrFail($id);
+        $blog->delete();
+        if($blog) {
+            Session::flash('status', 'success');
+            Session::flash('message', 'delete blog success !');
+        }
+        return redirect('/blog/'); // ke halaman sebelumnya
     }
 }
